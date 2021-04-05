0ee8b9566c9244dd3b27fbce1563f6080ceabd71
Muhammad Daffa' A.R <daffavcd@gmail.com>
2021-04-05 23:05:17 +0700

    list view sudah

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -2,12 +2,13 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:jobsheet9/models/movie.dart';
 
 class HttpService {
   final String apiKey = 'eee6c4bddcfc3ad6c1ab64c445e68585';
   final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
 
-  Future<String> getPopularMovies() async {
+  Future<List> getPopularMovies() async {
     final String uri = baseUrl + apiKey;
 
     http.Response result = await http.get(Uri.parse(uri));
@@ -15,8 +16,8 @@ class HttpService {
       print("Sukses");
       final jsonResponse = json.decode(result.body);
       final moviesMap = jsonResponse['result'];
-      String response = result.body;
-      return response;
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
     } else {
       print("Fail");
       return null;
